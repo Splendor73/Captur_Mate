7714119891740b44a562dd2cd5996279708fa5e6
Splendor73 <splendor7ed@gmail.com>
2024-10-26 01:02:14 -0700

    Initial Commit

---
+++ CapturMate/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CapturMate
+//
+//  Created by Yashu Patel on 10/26/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CapturMateTests/CapturMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  CapturMateTests.swift
+//  CapturMateTests
+//
+//  Created by Yashu Patel on 10/26/24.
+//
+
+import Testing
+@testable import CapturMate
+
+struct CapturMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
